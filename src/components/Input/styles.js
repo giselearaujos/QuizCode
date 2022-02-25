a3f3cc60de48da8e5186790a7f9715a9bc5e48cb
giselearaujos <giselearaujo07.s@gmail.com>
2022-02-25 16:23:49 -0300

    :sparkles: Create Input component

---
+++ src/components/Input/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  width: 50%;
+  height: 50px;
+  font-family: 'Press Start 2P';
+  font-weight: bold;
+  font-size: 16px;
+  color: ${(props) => props.theme.colors.primary};
+  text-shadow: 2px 2px ${(props) => props.theme.colors.secondary};
+  box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
+  border: 1px solid ${(props) => props.theme.colors.secondary};
+  border-radius: 10px;
+  padding: 0px 10px;
+`;
+
+export default Input;
