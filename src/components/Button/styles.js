fcff6c61ce728203172876d907a8ee92fd2dc97a
giselearaujos <giselearaujo07.s@gmail.com>
2022-02-25 16:48:35 -0300

    :sparkles: Create Button component

---
+++ src/components/Button/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  width: 54%;
+  height: 52px;
+  font-family: 'Press Start 2P';
+  font-weight: bold;
+  font-size: 16px;
+  background-color: ${(props) => props.theme.colors.backgroundColor};
+  color: ${(props) => props.theme.colors.white};
+  text-shadow: 2px 1px ${(props) => props.theme.colors.secondary};
+  box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
+  border: 1px solid ${(props) => props.theme.colors.secondary};
+  border-radius: 10px;
+  padding: 0px 10px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${(props) => props.theme.colors.third};
+  }
+`;
+
+export default Button;
