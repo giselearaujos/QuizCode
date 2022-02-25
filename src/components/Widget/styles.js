48830733e9ed7235e2a64805a7224712a6260520
giselearaujos <giselearaujo07.s@gmail.com>
2022-02-25 16:23:06 -0300

    :sparkles: Create Widget component

---
+++ src/components/Widget/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Widget = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+`;
+
+export default Widget;
