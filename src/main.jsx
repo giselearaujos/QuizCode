a6625be56033179a3ac5286633779c091034fcb7
giselearaujos <giselearaujo07.s@gmail.com>
2022-02-26 08:41:28 -0300

    :recycle: Modified code

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './Routes';
 import GlobalStyles from './styles/generic/Reset';
 import themes from './styles/themes/styles';
 import Base from './styles/elements/Base';
-import Card from './components/Card/styles';
-import Title from './components/Title/styles';
-import Widget from './components/Widget/styles';
-import Input from './components/Input/styles';
-import Button from './components/Button/styles';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <ThemeProvider theme={themes}>
       <GlobalStyles />
       <Base>
-        <Card>
-          <Title>QuizCODE</Title>
-          <Widget>
-            <Input placeholder="Nome" />
-            <Button>Entrar</Button>
-          </Widget>
-        </Card>
+        <Routes />
       </Base>
     </ThemeProvider>
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import Card from '../../components/Card/styles';
 import Title from '../../components/Title/styles';
-import Widget from '../../components/Widget/styles';
-import Input from '../../components/Input/styles';
-import Button from '../../components/Button/styles';
+import Form from '../../components/Form/index';
 
 const Home = () => {
   return (
     <Card>
       <Title>QuizCODE</Title>
-      <Widget onSubmit="">
-        <Input placeholder="Nome" />
-        <Button type="submit">Entra</Button>
-      </Widget>
+      <Form />
     </Card>
   );
 };
