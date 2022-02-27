4ac863618ccd5c53037472cfaa2d2488b01d190b
giselearaujos <giselearaujo07.s@gmail.com>
2022-02-27 15:03:16 -0300

    :sparkles: Create QuizHeader component

---
+++ src/components/QuizHeader/styles.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const QuizHeader = styled.head`
+  display: flex;
+  justify-content: space-between;
+  padding: 0px 30px;
+`;
+
+export default QuizHeader;
