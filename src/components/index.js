1bb8abc3c1449e86042218b90a785bd098f723ca
giselearaujos <giselearaujo07.s@gmail.com>
2022-02-28 15:32:06 -0300

    :recycle: created component index file

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Button } from './Button/styles';
+export { default as Card } from './Card/styles';
+export { default as Input } from './Input/styles';
+export { default as Title } from './Title/styles';
+export { default as QuizHeader } from './QuizHeader/styles';
+export { default as Score } from './Score/styles';
