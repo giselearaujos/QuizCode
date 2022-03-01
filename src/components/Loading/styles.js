efa7c203b1f92e32a063e2aeb8b9fc8aaa821ee0
giselearaujos <giselearaujo07.s@gmail.com>
2022-03-01 11:58:46 -0300

    :sparkles: Create Loading component

---
+++ src/components/Loading/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Loading = styled.div`
+  text-align: center;
+  font-size: 28px;
+  color: ${(props) => props.theme.colors.backgroundColor};
+`;
+export default Loading;
