251cb5bc72966e17813dd4f761c4255ef1892abb
giselearaujos <giselearaujo07.s@gmail.com>
2022-03-01 12:11:53 -0300

    :wrench: add player score in component

--- src/pages/Result/index.jsx
+++ src/pages/Result/index.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import { Card, QuizHeader, Score } from '../../components';
+import { useNavigate } from 'react-router-dom';
+import { Text, Button } from '../../components';
+import FormStyled from '../../components/Form/styles';
 
-const Result = () => {
+const Result = ({ score }) => {
+  const navigateTo = useNavigate();
+  const handleUserRedirect = () => navigateTo('/');
+
   return (
-    <Card height="80%">
-      <QuizHeader>
-        <Title fontSize="20px" marginTop="25px">
-          QuizCode
-        </Title>
-        <Title fontSize="20px" marginTop="25px">
-          Gisele
-        </Title>
-      </QuizHeader>
-      <Score>
-        <Title fontSize="15px" marginTop="25px">
-          RESULT 1/10
-        </Title>
-      </Score>
-    </Card>
+    <div>
+      <Text fontSize="18px" marginTop="25px">
+        Your result is {score}/10
+      </Text>
+      <FormStyled>
+        <Button type="submit" marginBottom="35px" onClick={handleUserRedirect}>
+          Return
+        </Button>
+      </FormStyled>
+    </div>
   );
 };
 
