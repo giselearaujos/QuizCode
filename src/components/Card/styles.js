836686f76eef4c1a5860f5b0f61d7e0840dd7477
giselearaujos <giselearaujo07.s@gmail.com>
2022-03-01 13:21:12 -0300

    :lipstick: add responsiveness in card component

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -7,6 +7,26 @@ const Card = styled.div`
   border-radius: 10px;
   box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
   min-height: 20%;
+
+  @media (min-width: 769px) and (max-width: 1200px) {
+    width: 82%;
+  }
+
+  @media (min-width: 581px) and (max-width: 768px) {
+    width: 83%;
+  }
+
+  @media (min-width: 481px) and (max-width: 580px) {
+    width: 87%;
+  }
+
+  @media (min-width: 320px) and (max-width: 480px) {
+    width: 94%;
+  }
+
+  @media (min-width: 200px) and (max-width: 321px) {
+    width: 92%;
+  }
 `;
 
 export default Card;
