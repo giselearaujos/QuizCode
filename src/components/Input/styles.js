56d8032fb4514895b643bd1eafee21a5908b9567
giselearaujos <giselearaujo07.s@gmail.com>
2022-03-01 13:43:14 -0300

    :lipstick: add responsiveness in input component

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -13,6 +13,31 @@ const Input = styled.input`
   border-radius: 10px;
   padding: 0px 10px;
   margin-bottom: 20px;
+
+  @media (min-width: 769px) and (max-width: 1200px) {
+    font-size: 25px;
+    width: 69%;
+  }
+
+  @media (min-width: 581px) and (max-width: 768px) {
+    font-size: 24px;
+    width: 62%;
+  }
+
+  @media (min-width: 481px) and (max-width: 580px) {
+    font-size: 18px;
+    width: 75%;
+  }
+
+  @media (min-width: 320px) and (max-width: 480px) {
+    font-size: 18px;
+    width: 85%;
+  }
+
+  @media (min-width: 200px) and (max-width: 321px) {
+    font-size: 18px;
+    width: 88%;
+  }
 `;
 
 export default Input;
